5697b5b608c239b573c4fb279183538b26bcf7b6
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-14 00:34:12 +0530

    Creare Fake profile example

---
+++ Create_Fakeprofile/Create_Fake_Profile.py
@@ -0,0 +1,26 @@
+from faker import Faker
+fake = Faker(['en_IN','en_US','fr_FR'])
+print(" ALL FAKER DETAILS ")
+print(dir(fake))
+
+summary = '''
+
+# Faker profile Summery 
+
+Name : {}
+Address : {}
+phone_number : {}
+Email : {}
+Social_Security_number(SSN) : {}
+Location : {},{}
+URL : {}
+'''. format(fake.name(),
+            fake.address(),
+             fake.phone_number(),
+             fake.email(),
+             fake.ssn(),
+             fake.latitude(),fake.longitude(),
+
+             fake.url())
+print(summary)
+
