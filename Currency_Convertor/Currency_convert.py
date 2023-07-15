8b5cb0e0f10f39b9e0144db0d8c8d600a7217f97
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-15 18:19:17 +0530

    code using tkinter package

--- Currency_Convertor/Currency_convert.py
+++ Currency_Convertor/Currency_convert.py
@@ -1,39 +1,82 @@
-from locale import currency
+# convert currency codes...
+#USD: United States Dollar
+# EUR: Euro
+# GBP: British Pound Sterling
+# JPY: Japanese Yen
+# CAD: Canadian Dollar
+# AUD: Australian Dollar
+# CHF: Swiss Franc
+# CNY: Chinese Yuan Renminbi
+# INR: Indian Rupee
+# MXN: Mexican Peso
+
+
+import tkinter as tk
 import requests
 import json
 
-currency_code = input("From_Currency_Code: ").lower()
-cache = {}
+class CurrencyConverter:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Currency Converter")
 
-while True:
-    currency_exchange = input("To_Currency_Code: ").lower()
+        self.currency_code_label = tk.Label(self.root, text="From_Currency_Code: ")
+        self.currency_code_label.pack()
+        self.currency_code_entry = tk.Entry(self.root)
+        self.currency_code_entry.pack()
 
-    if currency_exchange == '':
-        break
+        self.currency_exchange_label = tk.Label(self.root, text="To_Currency_Code: ")
+        self.currency_exchange_label.pack()
+        self.currency_exchange_entry = tk.Entry(self.root)
+        self.currency_exchange_entry.pack()
 
-    try:
-        amount_to_exchange = int(input("Amount : "))
+        self.amount_label = tk.Label(self.root, text="Amount: ")
+        self.amount_label.pack()
+        self.amount_entry = tk.Entry(self.root)
+        self.amount_entry.pack()
 
-    except ValueError:
-        print("Invalid input for amount. Please enter a valid number.")
-        continue
+        self.cache = {}
 
-    URL = f'http://www.floatrates.com/daily/{currency_code}.json'
-    exch = json.loads(requests.get(URL).text)
+        self.convert_button = tk.Button(self.root, text="Convert", command=self.convert_currency)
+        self.convert_button.pack()
 
-    if currency_code == 'usd':
-        cache.update(eur=exch['eur']['rate'])
-    elif currency_code == 'eur':
-        cache.update(usd=exch['usd']['rate'])
-    else:
-        cache.update(usd=exch['usd']['rate'], eur=exch['eur']['rate'])
+        self.result_label = tk.Label(self.root, text="")
+        self.result_label.pack()
 
-    print("Checking the cache...")
-    if currency_exchange in cache:
-        rate = round(amount_to_exchange * cache[currency_exchange], 2)
-        print("Oh! It is in the cache!")
-        print(f"You received {rate} {currency_exchange.upper()}.")
-    else:
-        cache[currency_exchange] = exch[currency_exchange]['rate']
-        rate = round(amount_to_exchange * cache[currency_exchange], 2)
-        print(f"You received {rate} {currency_exchange.upper()}.")
+    def convert_currency(self):
+        currency_code = self.currency_code_entry.get().lower()
+        currency_exchange = self.currency_exchange_entry.get().lower()
+
+        if currency_exchange == '':
+            return
+
+        try:
+            amount_to_exchange = int(self.amount_entry.get())
+        except ValueError:
+            self.result_label.config(text="Invalid input for amount. Please enter a valid number.")
+            return
+
+        URL = f'http://www.floatrates.com/daily/{currency_code}.json'
+        exch = json.loads(requests.get(URL).text)
+
+        if currency_code == 'usd':
+            self.cache.update(eur=exch['eur']['rate'])
+        elif currency_code == 'eur':
+            self.cache.update(usd=exch['usd']['rate'])
+            
+
+        else:
+            self.cache.update(usd=exch['usd']['rate'], eur=exch['eur']['rate'])
+
+        print("Checking the cache...")
+        if currency_exchange in self.cache:
+            rate = round(amount_to_exchange * self.cache[currency_exchange], 2)
+            self.result_label.config(text=f"Oh! It is in the cache!\nYou received {rate} {currency_exchange.upper()}.", fg = "green")
+        else:
+            self.cache[currency_exchange] = exch[currency_exchange]['rate']
+            rate = round(amount_to_exchange * self.cache[currency_exchange], 2)
+            self.result_label.config(text=f"You received {rate} {currency_exchange.upper()}.",fg = "red"  )
+
+root = tk.Tk()
+converter = CurrencyConverter(root)
+root.mainloop()
