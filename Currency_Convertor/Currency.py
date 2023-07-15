53fb4a7010181d6c608862942e6f05fe9707e3a3
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-15 16:57:38 +0530

    checking code

--- Currency_Convertor/Currency.py
+++ Currency_Convertor/Currency.py
@@ -1,28 +1,28 @@
+User
+# Convert the Currency to one to another Country
+#import the modules we need for coding
 import requests
 
-class CurrencyConverter:
+class Currency_convertor:
+    #declare empty dict to store the convertion rates
     rates = {}
-
-    def __init__(self, url):
+    def __init__(self,url):
         data = requests.get(url).json()
+        #Extract only rates from json data
         self.rates = data["rates"]
 
-    def convert(self, from_currency, to_currency, amount):
-        initial_amount = amount
+    def Convertor(self,From_currency , To_currency , Amount):
+        initial_amount = Amount
+        if From_currency != "EUR":
+            Amount = Amount / self.rates[From_currency]
+        Amount = round(Amount * self.rates[To_currency],2)
+        print(' {}{} = {}{} '.format(initial_amount,From_currency,Amount,To_currency))
 
-        if from_currency != "EUR":
-            amount = amount / self.rates[from_currency]
+if '__name__' == '__main__' :
 
-        amount = round(amount * self.rates[to_currency], 2)
-        print('{}{} = {}{}'.format(initial_amount, from_currency, amount, to_currency))
-
-if __name__ == '__main__':
-    YOUR_ACCESS_KEY = "YOUR_ACCESS_KEY"
-    url = f'http://data.fixer.io/api/latest?access_key={YOUR_ACCESS_KEY}'
-    converter = CurrencyConverter(url)
-
-    from_currency = input("FROM CURRENCY: ")
-    to_currency = input("TO CURRENCY: ")
-    amount = int(input("AMOUNT: "))
-
-    converter.convert(from_currency, to_currency, amount)
+    url = str.__add__('http://data.fixer.io/api/latest?access_key', YOUR_ACCESS_KEY)
+    c = Currency_convertor(url)
+    From_currency = input("FROM CURRENCY : ")
+    To_currency = input("TO CURRENCY : ")
+    Amount = int(input("AMOUNT : "))
+    c.Convertor(From_currency , To_currency , Amount)
