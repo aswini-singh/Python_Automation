083acf8f771d358e242c0edb6f3c9ff53d3905d6
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-15 16:47:30 +0530

    adding lines of code

---
+++ Currency_Convertor/Currency.py
@@ -0,0 +1,28 @@
+import requests
+
+class CurrencyConverter:
+    rates = {}
+
+    def __init__(self, url):
+        data = requests.get(url).json()
+        self.rates = data["rates"]
+
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+
+        if from_currency != "EUR":
+            amount = amount / self.rates[from_currency]
+
+        amount = round(amount * self.rates[to_currency], 2)
+        print('{}{} = {}{}'.format(initial_amount, from_currency, amount, to_currency))
+
+if __name__ == '__main__':
+    YOUR_ACCESS_KEY = "YOUR_ACCESS_KEY"
+    url = f'http://data.fixer.io/api/latest?access_key={YOUR_ACCESS_KEY}'
+    converter = CurrencyConverter(url)
+
+    from_currency = input("FROM CURRENCY: ")
+    to_currency = input("TO CURRENCY: ")
+    amount = int(input("AMOUNT: "))
+
+    converter.convert(from_currency, to_currency, amount)
