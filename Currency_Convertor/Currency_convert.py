b366c27fb840b864f59a793970e28e1830e7d7b0
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-17 19:51:13 +0530

    delete and fix errors

--- Currency_Convertor/Currency_convert.py
+++ Currency_Convertor/Currency_convert.py
@@ -1,18 +1,20 @@
 import tkinter as tk
 import requests
 import json
+
+
 class CurrencyConverter:
     def __init__(self, root):
         self.root = root
         self.root.title("Currency Converter")
-        self.cache = {} 
+        self.cache = {}
 
         # Create a frame to hold the labels and entry widgets
         input_frame = tk.Frame(self.root)
         input_frame.pack(padx=20, pady=10)
 
         self.currency_code_label = tk.Label(input_frame, text="FromCurrencyCode:")
-        self.currency_code_label.grid(row=0,column=0,padx=5, pady=5)
+        self.currency_code_label.grid(row=0, column=0, padx=5, pady=5)
         self.currency_code_entry = tk.Entry(input_frame)
         self.currency_code_entry.grid(row=0, column=1, padx=5, pady=5)
 
@@ -29,10 +31,9 @@ class CurrencyConverter:
         self.convert_button = tk.Button(self.root, text="Convert", command=self.convert_currency)
         self.convert_button.pack()
 
-        self.result_label = tk.Label(self.root,text="")
+        self.result_label = tk.Label(self.root, text="")
         self.result_label.pack()
 
-    
     def convert_currency(self):
         currency_code = self.currency_code_entry.get().lower()
         currency_exchange = self.currency_exchange_entry.get().lower()
@@ -47,21 +48,33 @@ class CurrencyConverter:
             return
 
         URL = f'http://www.floatrates.com/daily/{currency_code}.json'
-        exch = json.loads(requests.get(URL).text)
+        response = requests.get(URL)
+        if response.status_code != 200:
+            self.result_label.config(text="Failed to fetch exchange rates. Please try again later.")
+            return
 
-        if currency_code == 'usd':
-            self.cache.update(eur=exch['eur']['rate'])
-        else:
-            self.cache.update(usd=exch['usd']['rate'], eur=exch['eur']['rate'])
+        try:
+            exch = response.json()
+            if currency_code == 'eur':
+                self.cache.update(inr=exch['inr']['rate'])
+            else:
+                self.cache.update(inr=exch['inr']['rate'], eur=exch['eur']['rate'])
 
-        print("Checking the cache...")
-        if currency_exchange in self.cache:
-            rate = round(amount_to_exchange * self.cache[currency_exchange], 2)
-            self.result_label.config(text=f"Hi! You received {rate} {currency_exchange.upper()}.",fg="green")
-        else:
-            self.cache[currency_exchange] = exch[currency_exchange]['rate']
-            rate = round(amount_to_exchange * self.cache[currency_exchange], 2)
-            self.result_label.config(text=f"You received {rate} {currency_exchange.upper()}.", fg="red")
+            print("Checking the cache...")
+            if currency_exchange in self.cache:
+                rate = round(amount_to_exchange * self.cache[currency_exchange], 2)
+                self.result_label.config(text=f"Hi! You will receive {rate} {currency_exchange.upper()}.",
+                                         fg="green")
+            else:
+                if currency_exchange in exch:
+                    self.cache[currency_exchange] = exch[currency_exchange]['rate']
+                    rate = round(amount_to_exchange * self.cache[currency_exchange], 2)
+                    self.result_label.config(text=f"You will receive {rate} {currency_exchange.upper()}.", fg="red")
+                else:
+                    self.result_label.config(text="Invalid currency code. Please enter a valid currency code.")
+        except (json.JSONDecodeError, KeyError):
+            self.result_label.config(text="Failed to process exchange rates. Please try again later.")
+
 
 root = tk.Tk()
 converter = CurrencyConverter(root)
