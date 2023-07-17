3ca9a419378800985d3a6ecb7a5addcdf5b80c3c
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-17 19:52:24 +0530

    adding new

---
+++ NumberGuessingGame/NumberGuess.py
@@ -0,0 +1,23 @@
+# Guess the number game in Python
+import random
+print("The Number Guessing Game")
+#computer will select random number
+num = random.randint(1,20)
+
+chances =0
+print("Guess a Number between 1 to 19")
+while chances < 5:
+    guess = int(input("enter any number: "))
+    if guess == num:
+        print("congrats! you won")
+        break
+    elif guess > num :
+        print("your guess is higher. pls select a lower number", guess)
+    else:
+        print("your guess is lower. pls select higher number",guess)
+    chances +=1
+
+if not chances > 5:
+    print("sorry! you losse to guess")
+
+
