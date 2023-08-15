1ad346c1ba6566349ff65d75594994e96f668760
aswini-singh <aswiniamarsingh@gmail.com>
2023-08-15 13:14:36 +0530

    fix errors

--- flag/Flag.py
+++ flag/Flag.py
@@ -3,6 +3,17 @@ import math
 
 root = tk.Tk()
 root.title("")
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+window_width = 600
+window_height = 450
+x_position = (screen_width - window_width) // 2
+y_position = (screen_height - window_height) // 2
+
+root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+
 canvas = tk.Canvas(root, width=600, height=450)  
 canvas.pack()
 
